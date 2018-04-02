3e00ebdca9f9cf15b9536fe985ad8c7c2b4dfc09
Pepdoll <36697683+Pepdoll@users.noreply.github.com>
2018-04-02 18:01:06 +0400

    Add files via upload

--- parser.cpp
+++ parser.cpp
@@ -9,6 +9,7 @@ std::vector<tok> parser::parse(std::string s) {
 		if (s[i] >= '0' && s[i] <= '9') {
 			int index = 0;
 			char p[10]=" ";
+			int j = i;
 			while (s[i] >= '0' && s[i] <= '9' || s[i] == ','||s[i]=='.') {
 				if (s[i] == '.') s[i] = ',';
 				p[index] = s[i];
@@ -17,9 +18,14 @@ std::vector<tok> parser::parse(std::string s) {
 			}
 			i--;
 			double m = atof(p);
-			res.push_back(tok::tok(atof(p)));
+			if (j > 0 && s[j - 1] == '-') {
+				m *= -1;
+				res.erase(res.end() - 1);
+			}
+			res.push_back(tok::tok(m));
 		}
 		else if (s[i] == '+'|| s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '(' || s[i] == ')') {
+			if (i > 0 && (s[i - 1] == '*' || s[i - 1] == '/'))continue;
 			res.push_back(tok::tok(s[i]));
 		}
 		else {
